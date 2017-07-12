9a0de4fcaa7e69a7a9f4e549fbad352822ae12d3
erwinwahyura <you@example.com>
2017-07-13 01:11:16 +0700

    flat model mongodb 3 collections

---
+++ server/models/user.js
@@ -0,0 +1,28 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var validator = require("email-validator");
+
+var userSchema = new Schema({
+  name: {
+    type: String,
+    required: [true, 'name is required']
+  },
+  username:  {
+    type : String,
+    required : [true, 'username is required! please fill it!'],
+    unique : [true, 'username already used by another user']
+  },
+  password: {
+    type : String,
+    required : [true, 'password is required! please fill it!']
+  },
+  email: {
+    type : String,
+    required : [true, 'email is required! please fill it!']
+
+  }
+});
+
+var user = mongoose.model('User', userSchema);
+
+module.exports = user
