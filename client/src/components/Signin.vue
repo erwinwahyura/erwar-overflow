e3d40aa16dd1f672b89dfbe70ff34428a0f02bbd
erwinwahyura <you@example.com>
2017-07-16 19:11:50 +0700

    auth

---
+++ client/src/components/Signin.vue
@@ -0,0 +1,133 @@
+<template lang="html">
+  <div class="bg column is-4 is-offset-4">
+    <div class="content-register">
+      <h1>LOGIN HERE</h1>
+      <hr>
+      <div class="field">
+        <label class="label">Username</label>
+        <p class="control has-icons-left has-icons-right">
+          <input class="input is-success" type="text" placeholder="Text input" required="" v-model="dataUser.username">
+          <span class="icon is-small is-left">
+            <i class="fa fa-user"></i>
+          </span>
+          <span class="icon is-small is-right">
+            <i class="fa fa-check"></i>
+          </span>
+        </p>
+        <p class="help is-success">This username is available</p>
+      </div>
+
+      <div class="field">
+        <label class="label">Password</label>
+        <p class="control has-icons-left has-icons-right">
+          <input class="input is-success" type="password" placeholder="Text input" required="" v-model="dataUser.password">
+          <span class="icon is-small is-left">
+            <i class="fa fa-user"></i>
+          </span>
+          <span class="icon is-small is-right">
+            <i class="fa fa-check"></i>
+          </span>
+        </p>
+        <p class="help is-success">This password is available</p>
+      </div>
+
+      <div class="field is-grouped">
+        <div class="column is-12">
+          <button type="button" class="button is-primary" name="button" @click="doLogin()">Sign Up</button>
+        </div>
+      </div>
+  </div>
+
+  </div>
+
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      dataUser: {
+        username: '',
+        password: ''
+      }
+    }
+  },
+  methods: {
+    doLogin () {
+      var self = this
+      this.axios.post('http://localhost:3000/api/users/signin', {
+        username: self.dataUser.username,
+        password: self.dataUser.password
+      })
+      .then((response) => {
+        self.dataUser.username = ''
+        self.dataUser.password = ''
+        console.log(response.data)
+        if (response.data) {
+          console.log(response.data)
+          var token = response.data.token
+          localStorage.setItem('token', token)
+          localStorage.setItem('name', response.data.name)
+          localStorage.setItem('unique', response.data.id)
+          this.$router.push('/')
+        }
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+.content{
+  text-align: left;
+  margin-left: 200px;
+  color: #fff;
+  font-weight: bolder;
+}
+h1 {
+  color: #fff;
+}
+.columns.content-full {
+  padding-top: 50px;
+  padding-left: 50px;
+  padding-right: 50px;
+  padding-bottom: 0px;
+  background-color: #108AEC;
+}
+label {
+  text-align: left;
+  color: white;
+}
+.content-register {
+  line-height: 0.1rem;
+  padding-left: 50px;
+  padding-right: 50px;
+  padding-bottom: 50px;
+}
+div.column.is-12 {
+  padding-top: 10px;
+  padding-left: 0px;
+  padding-right: 0px;
+  padding-bottom: 0px;
+}
+button.button.is-primary {
+    width: 100%;
+}
+.bg {
+  padding-top: 60px;
+  /*background-color: green;*/
+  border: 1px solid #e4e6e8;
+}
+.bg.column.is-4.is-offset-4 {
+    margin-top: 100px;
+}
+label {
+  color: black;
+}
+h1 {
+  color: black;
+}
+</style>
---
+++ client/src/components/Signup.vue
@@ -0,0 +1,157 @@
+<template lang="html">
+  <div class="columns content-full">
+    <div class="column is-half">
+      <div class="content">
+        <h1>Learn, Share, Build</h1>
+        <p>Each month, over 50 million developers come to Stack Overflow to learn, share their knowledge, and build their careers.</p>
+        <p>Join the world’s largest developer community.</p>
+      </div>
+    </div>
+    <div class="column is-half">
+      <h1>REGISTRATION HERE</h1>
+      <div class="content-register">
+        <div class="field">
+          <label class="label">Name</label>
+          <p class="control">
+            <input class="input" type="text" placeholder="Text input" v-model="register.name" required>
+          </p>
+        </div>
+
+        <div class="field">
+          <label class="label">Email</label>
+          <p class="control has-icons-left has-icons-right">
+            <input class="input is-danger" type="text" placeholder="Email input" value="" v-model="register.email" required>
+            <span class="icon is-small is-left">
+              <i class="fa fa-envelope"></i>
+            </span>
+            <span class="icon is-small is-right">
+              <i class="fa fa-warning"></i>
+            </span>
+          </p>
+          <p class="help is-danger">This email is invalid</p>
+        </div>
+
+        <div class="field">
+          <label class="label">Username</label>
+          <p class="control has-icons-left has-icons-right">
+            <input class="input is-success" type="text" placeholder="Text input" v-model="register.username" required>
+            <span class="icon is-small is-left">
+              <i class="fa fa-user"></i>
+            </span>
+            <span class="icon is-small is-right">
+              <i class="fa fa-check"></i>
+            </span>
+          </p>
+          <p class="help is-success">This username is available</p>
+        </div>
+
+        <div class="field">
+          <label class="label">Password</label>
+          <p class="control has-icons-left has-icons-right">
+            <input class="input is-success" type="password" placeholder="Text input" v-model="register.password" required>
+            <span class="icon is-small is-left">
+              <i class="fa fa-user"></i>
+            </span>
+            <span class="icon is-small is-right">
+              <i class="fa fa-check"></i>
+            </span>
+          </p>
+          <p class="help is-success">This username is available</p>
+        </div>
+
+        <div class="field is-grouped">
+          <div class="is-primary">
+            <button type="button" class="column is-12 button is-primary"  @click="doRegister()">Sign Up</button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </div>
+
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      register: {
+        name: '',
+        username: '',
+        email: '',
+        password: ''
+      }
+    }
+  },
+  methods: {
+    doRegister () {
+      var self = this
+      this.axios.post('http://localhost:3000/api/users/signup', {
+        name: self.register.name,
+        username: self.register.username,
+        email: self.register.email,
+        password: self.register.password
+      })
+      .then((response) => {
+        console.log(response.data)
+        if (response.data.name !== '' && response.data.username !== '' && response.data.email !== '' && response.data.password !== '') {
+          alert('Sign Up Success \n' + 'Welcome :' + response.data.name)
+          this.$router.push('/login')
+        } else {
+          alert('Sign Up Failed, Check Your Input Please')
+        }
+        self.register.name = ''
+        self.register.username = ''
+        self.register.email = ''
+        self.register.password = ''
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+    },
+    coba () {
+      alert('hio')
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+.content{
+  text-align: left;
+  margin-left: 200px;
+  color: #fff;
+  font-weight: bolder;
+}
+h1 {
+  color: #fff;
+}
+.columns.content-full {
+  padding-top: 50px;
+  padding-left: 50px;
+  padding-right: 50px;
+  padding-bottom: 0px;
+  background-color: #108AEC;
+}
+label {
+  text-align: left;
+  color: white;
+}
+.content-register {
+  line-height: 0.1rem;
+  padding-left: 50px;
+  padding-right: 50px;
+  padding-bottom: 50px;
+}
+button.column.is-12.button.is-primary {
+    width: 653%;
+}
+.columns.content-full {
+  position: relative;
+  top: 14px;
+}
+header {
+    /*border-top: 3px solid #F48024;*/
+    /*border-bottom: 1px solid #e4e6e8;*/
+}
+</style>
